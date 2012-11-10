670c842ecb3489f478b4e0ae667e31f52634a09a
Christopher Roy Bratusek <nano@tuxfamily.org>
2012-11-10 18:59:20 +0100

    german translation

--- i18n/Qwad_de.ts
+++ i18n/Qwad_de.ts
@@ -1,31 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0">
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
 <context>
     <name>@default</name>
     <message>
         <location filename="../GUI/VenPri.py" line="49"/>
         <source>Unknown error</source>
-        <translation type="unfinished"></translation>
+        <translation>Unbekannter Fehler</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="381"/>
         <source>Downloading files from NUS... This may take a while, please, be patient.</source>
-        <translation type="unfinished"></translation>
+        <translation>Von NUS herunterladen... Dies kann eine Zeit lang dauern, bitte warten.</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="382"/>
         <source>Unpacking WAD...</source>
-        <translation type="unfinished"></translation>
+        <translation>WAD wird entpackt...</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="383"/>
         <source>Packing into WAD...</source>
-        <translation type="unfinished"></translation>
+        <translation>WAD wird gepackt...</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="384"/>
         <source>Cleaning temporary files...</source>
-        <translation type="unfinished"></translation>
+        <translation>Temporäre Dateien entfernen...</translation>
     </message>
 </context>
 <context>
@@ -33,7 +34,7 @@
     <message>
         <location filename="../GUI/Ui_AboutQwad.py" line="66"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Dialog</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_AboutQwad.py" line="67"/>
@@ -42,37 +43,37 @@
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:9pt; font-weight:300; font-style:normal;&quot;&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:22pt; font-weight:600;&quot;&gt;Qwad&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../GUI/Ui_AboutQwad.py" line="72"/>
         <source>Version</source>
-        <translation type="unfinished"></translation>
+        <translation>Version</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_AboutQwad.py" line="73"/>
         <source>Author: </source>
-        <translation type="unfinished"></translation>
+        <translation>Author:</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_AboutQwad.py" line="74"/>
         <source>GNU General Public License v3</source>
-        <translation type="unfinished"></translation>
+        <translation>GNU General Public License v3</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_AboutQwad.py" line="75"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation>Lizenz</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_AboutQwad.py" line="76"/>
         <source>Thanks to Wii.py team: Xuzz, SquidMan, megazig, TheLemonMan, |Omega, and Matt_P.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dank an Wii.py team: Xuzz, SquidMan, megazig, TheLemonMan, JOmega und Matt_P.</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_AboutQwad.py" line="77"/>
         <source>Thanks</source>
-        <translation type="unfinished"></translation>
+        <translation>Dank</translation>
     </message>
 </context>
 <context>
@@ -80,72 +81,72 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../GUI/VenPri.py" line="28"/>
         <source>(Latest)</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="45"/>
         <source>Ready</source>
-        <translation type="unfinished"></translation>
+        <translation>Bereit</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="51"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="51"/>
         <source>An error has ocurred:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ein Fehler trat auf:</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="79"/>
         <source>Select WAD file</source>
-        <translation type="unfinished"></translation>
+        <translation>WAD Datei auswählen</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="194"/>
         <source>*.wad; *.WAD</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="93"/>
         <source>Select where to store WAD contents</source>
-        <translation type="unfinished"></translation>
+        <translation>Auswählen wohin die WAD Inhalte gespeichert werden sollen</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="117"/>
         <source>Select where to save the newly created WAD</source>
-        <translation type="unfinished"></translation>
+        <translation>Auswählen wohin die neue WAD gespeichert werden soll</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="131"/>
         <source>Select folder to pack into WAD</source>
-        <translation type="unfinished"></translation>
+        <translation>Ordner zum Packen als WAD auswählen</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="166"/>
         <source>About Qt</source>
-        <translation type="unfinished"></translation>
+        <translation>Über Qt</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="194"/>
         <source>Select where to save the downloaded WAD</source>
-        <translation type="unfinished"></translation>
+        <translation>Auswählen wohin die heruntergeladene WAD gespeichert werden soll</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="201"/>
         <source>Select the directory where store the downloaded files</source>
-        <translation type="unfinished"></translation>
+        <translation>Verzeichnis zum Speichern der heruntergeladenen Dateien auswählen</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="299"/>
         <source>Select a TMD file</source>
-        <translation type="unfinished"></translation>
+        <translation>Eine TMD Datei auswählen</translation>
     </message>
     <message>
         <location filename="../GUI/VenPri.py" line="299"/>
         <source>tmd; TMD</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -153,192 +154,194 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="237"/>
         <source>Qwad - a WAD management tool</source>
-        <translation type="unfinished"></translation>
+        <translation>Qwad - Ein Werkzeug zum Verwalten von WAD Dateien</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="244"/>
         <source>Ruta hacia el archivo WAD que quieres desempaquetar</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Tooltip</translatorcomment>
+        <translation>WAD Datei auswählen</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="239"/>
         <source>WAD file</source>
-        <translation type="unfinished"></translation>
+        <translation>WAD Datei</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="240"/>
         <source>Carpeta donde se almacenaran los contenidos del archivo WAD</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Tooltip</translatorcomment>
+        <translation>Auswählen wohin die Inhalte der WAD Datei gespeichert werden sollen</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="265"/>
         <source>Output</source>
-        <translation type="unfinished"></translation>
+        <translation>Verzeichnis</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="242"/>
         <source>Unpack</source>
-        <translation type="unfinished"></translation>
+        <translation>Entpacken</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="243"/>
         <source>Unpack WAD</source>
-        <translation type="unfinished"></translation>
+        <translation>WAD entpacken</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="245"/>
         <source>TMD file</source>
-        <translation type="unfinished"></translation>
+        <translation>TMD Datei</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="246"/>
         <source>Title ID:</source>
-        <translation type="unfinished"></translation>
+        <translation>Titel ID:</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="247"/>
         <source>Title ID (ascii):</source>
-        <translation type="unfinished"></translation>
+        <translation>Titel ID (Ascii):</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="248"/>
         <source>Title Version:</source>
-        <translation type="unfinished"></translation>
+        <translation>Titel Version:</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="249"/>
         <source>Title Type:</source>
-        <translation type="unfinished"></translation>
+        <translation>Titel Typ:</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="250"/>
         <source>Required IOS:</source>
-        <translation type="unfinished"></translation>
+        <translation>Benötigtes IOS:</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="251"/>
         <source>Group ID:</source>
-        <translation type="unfinished"></translation>
+        <translation>Gruppen ID:</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="252"/>
         <source>Boot index:</source>
-        <translation type="unfinished"></translation>
+        <translation>Boot Index:</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="253"/>
         <source>Access rights:</source>
-        <translation type="unfinished"></translation>
+        <translation>Zugriffsrechte:</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="254"/>
         <source>Contents:</source>
-        <translation type="unfinished"></translation>
+        <translation>Inhalte:</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="255"/>
         <source>Reserved:</source>
-        <translation type="unfinished"></translation>
+        <translation>Reserviert:</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="256"/>
         <source>TMD viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>TMD Anzeige</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="264"/>
         <source>Carpeta y nombre del nuevo WAD</source>
-        <translation type="unfinished"></translation>
+        <translation>Pfad zur neuen WAD Datei</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="259"/>
         <source>Ruta hacia la carpeta que empaquetar en WAD</source>
-        <translation type="unfinished"></translation>
+        <translation>Als WAD zu packendes Verzeichnis</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="260"/>
         <source>Folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Verzeichnis</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="261"/>
         <source>Pack</source>
-        <translation type="unfinished"></translation>
+        <translation>Packen</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="262"/>
         <source>Pack WAD</source>
-        <translation type="unfinished"></translation>
+        <translation>WAD Packen</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="263"/>
         <source>Download</source>
-        <translation type="unfinished"></translation>
+        <translation>Herunterladen</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="266"/>
         <source>Title ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Titel ID</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="267"/>
         <source>Choose title:</source>
-        <translation type="unfinished"></translation>
+        <translation>Titel auswählen:</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="268"/>
         <source>Choose IOS</source>
-        <translation type="unfinished"></translation>
+        <translation>IOS auswählen</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="269"/>
         <source>Choose Region First!</source>
-        <translation type="unfinished"></translation>
+        <translation>Region auswählen!</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="270"/>
         <source>Choose Region</source>
-        <translation type="unfinished"></translation>
+        <translation>Region auswählen</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="271"/>
         <source>Decrypt</source>
-        <translation type="unfinished"></translation>
+        <translation>Decrypt</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="272"/>
         <source>Pack as WAD</source>
-        <translation type="unfinished"></translation>
+        <translation>WAD packen</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="273"/>
         <source>Version:</source>
-        <translation type="unfinished"></translation>
+        <translation>Version:</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="274"/>
         <source>Available Versions:</source>
-        <translation type="unfinished"></translation>
+        <translation>Verf. Versionen:</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="275"/>
         <source>Download from NUS</source>
-        <translation type="unfinished"></translation>
+        <translation>Von NUS laden</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="276"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Hilfe</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="277"/>
         <source>About Qwad</source>
-        <translation type="unfinished"></translation>
+        <translation>Über Qwad</translation>
     </message>
     <message>
         <location filename="../GUI/Ui_VenPri.py" line="278"/>
         <source>About Qt</source>
-        <translation type="unfinished"></translation>
+        <translation>Über Qt</translation>
     </message>
 </context>
 </TS>
